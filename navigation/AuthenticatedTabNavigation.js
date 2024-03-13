41c9b883a1dbb159a10b8c4157f384305aa12e00
Anil <anil.kumar@shellinfoglobal.com>
2024-03-13 20:02:43 +0530

    Bottom Navigation styles changed

--- navigation/AuthenticatedTabNavigation.js
+++ navigation/AuthenticatedTabNavigation.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
@@ -7,6 +7,32 @@ import LocationSearchScreen from "../screens/LocationSearchScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
+
+const CustomTabBarButton = (props) => {
+  const { children, onPress, accessibilityState } = props;
+
+  return (
+    <Pressable
+      android_ripple={{ color: Colors.shadowColor }}
+      style={({ pressed }) => pressed && styles.pressedBtn}
+      onPress={onPress}
+    >
+      <View
+        style={[
+          styles.customTabInnerContainer,
+          styles.shadow,
+          {
+            backgroundColor: accessibilityState.selected
+              ? Colors.black
+              : Colors.primary800,
+          },
+        ]}
+      >
+        {children}
+      </View>
+    </Pressable>
+  );
+};
 
 const AuthenticatedTabNavigation = () => {
   return (
@@ -18,11 +44,13 @@ const AuthenticatedTabNavigation = () => {
         // unmountOnBlur: true,
         tabBarStyle: {
           position: "absolute",
-          bottom: 0,
-          right: 0,
-          left: 0,
+          bottom: 10,
+          right: 20,
+          left: 20,
           elevation: 0,
-          height: 50,
+          height: 70,
+          borderRadius: 15,
+          ...styles.shadow,
         },
       }}
     >
@@ -31,11 +59,22 @@ const AuthenticatedTabNavigation = () => {
         component={HomeScreen}
         options={{
           tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? "home" : "home-outline"}
-              size={24}
-              color={focused ? Colors.primary800 : Colors.gray}
-            />
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? Colors.primary800 : Colors.gray}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontFamily: "semibold",
+                  color: focused ? Colors.primary800 : Colors.gray,
+                }}
+              >
+                HOME
+              </Text>
+            </View>
           ),
         }}
       />
@@ -43,13 +82,14 @@ const AuthenticatedTabNavigation = () => {
         name="LocationSearch"
         component={LocationSearchScreen}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: () => (
             <Ionicons
-              name={"search-sharp"}
-              size={24}
-              color={focused ? Colors.primary800 : Colors.gray}
+              name={"location-outline"}
+              size={30}
+              color={Colors.white}
             />
           ),
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -57,11 +97,22 @@ const AuthenticatedTabNavigation = () => {
         component={ProfileScreen}
         options={{
           tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name={focused ? "person" : "person-outline"}
-              size={24}
-              color={focused ? Colors.primary800 : Colors.gray}
-            />
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Ionicons
+                name={focused ? "person" : "person-outline"}
+                size={24}
+                color={focused ? Colors.primary800 : Colors.gray}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontFamily: "semibold",
+                  color: focused ? Colors.primary800 : Colors.gray,
+                }}
+              >
+                PROFILE
+              </Text>
+            </View>
           ),
         }}
       />
@@ -71,4 +122,26 @@ const AuthenticatedTabNavigation = () => {
 
 export default AuthenticatedTabNavigation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: Colors.gray,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 4,
+  },
+  customTabInnerContainer: {
+    top: -30,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  pressedBtn: {
+    opacity: 0.25,
+  },
+});
