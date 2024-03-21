82068d6e31b8923d125d73e1f6939b475521905f
Anil <anil.kumar@shellinfoglobal.com>
2024-03-21 22:11:52 +0530

    Margin changed for custom switch for ios

--- components/ui/paper/CustomSwitch.js
+++ components/ui/paper/CustomSwitch.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginHorizontal: 8,
-    marginBottom: Platform.select({ ios: 12 }),
+    marginVertical: Platform.select({ ios: 12 }),
   },
   switchText: {
     fontFamily: "medium",
