91c179823bfb56ac04e9b83bc4c1d0444330d339
Anil <anil.kumar@shellinfoglobal.com>
2024-04-24 09:32:48 +0530

    timeout time changed in axios

--- util/axios.js
+++ util/axios.js
@@ -4,7 +4,7 @@ export const baseURL = "http://13.232.201.89:8001";
 
 export default axios.create({
   baseURL: baseURL,
-  timeout: 5000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
